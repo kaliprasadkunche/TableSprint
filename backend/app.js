780c9c75fb0111f36b1e567c7d0e36523738145b
kaliprasadkunche <113325469+kaliprasadkunche@users.noreply.github.com>
2024-09-15 19:32:20 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// const db = mysql2.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Kali659600',
+//     database: 'tablesprint' 
+// });
+
 const db = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Kali659600',
-    database: 'tablesprint' 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
