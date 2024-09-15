70db0e2592fb2066e9733e3aa20d8070ec65cb75
kaliprasadkunche <113325469+kaliprasadkunche@users.noreply.github.com>
2024-09-15 20:17:41 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ const createToken = (id) => {
 
 // Create categories table if it does not exist
 const createUsersTable = `
-CREATE TABLE IF NOT EXISTSusers (
+CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
